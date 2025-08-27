56a2b5138796434f8b507c7eb0cc372bec586498
Pedro Flores <pedro_23_96@hotmail.com>
2025-08-27 18:41:20 +0200

    fix: circle photo animation

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -20,7 +20,12 @@ export const Photo = () => {
     <motion.svg className="w-[300px] xl:w-[506px] h-[300px] xl:h-[506px]"
       fill="transparent"
       viewBox="0 0 506 506"
-      xmlns="http://www.w3.org/2000/svg">
+      xmlns="http://www.w3.org/2000/svg"
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1
+      }}
+      transition={{delay: 2, duration: 0.4, ease: "easeInOut" }}>
       <motion.circle
         cx="253"
         cy="253"
