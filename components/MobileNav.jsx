87e0628a4d753c33e9ac1a98678728e12c84eb31
Pedro Flores <pedro_23_96@hotmail.com>
2025-08-28 18:33:48 +0200

    feat: work page

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -26,7 +26,7 @@ const links = [
     },
     {
         name: "contact",
-        path: "contact"
+        path: "/contact"
     },
 ]
 
--- components/Photo.jsx
+++ components/Photo.jsx
@@ -13,7 +13,7 @@ export const Photo = () => {
       }}
       transition={{delay: 2, duration: 0.4, ease: "easeInOut" }}
       className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten absolute">
-      <Image src="/photo.png" priority quality={100} fill alt="" className="object-contain" />
+      <Image src="/assets/photo.png" priority quality={100} fill alt="" className="object-contain" />
     </motion.div>
 
     {/* Circle */}
